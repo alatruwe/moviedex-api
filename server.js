9cbc1ab29f65f148b29cdde7179c6f459e7aeb87
adelinel <adeline.latruwe@gmail.com>
2021-01-18 22:05:42 -0800

    added handlegetMovie middleware for search options

--- server.js
+++ server.js
@@ -10,9 +10,32 @@ app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
 
-app.use((req, res) => {
-  res.send("Hello, you!");
-});
+function handleGetMovie(req, res) {
+  let response = MOVIEDEX;
+  // filter by genre if genre is present
+  if (req.query.genre) {
+    response = response.filter((movie) =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    );
+  }
+  // filter by country if country is present
+  if (req.query.country) {
+    response = response.filter((movie) =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    );
+  }
+
+  // filter by avg_vote if avg_vote is present
+  if (req.query.avg_vote) {
+    response = response.filter(
+      (movie) => Number(movie.avg_vote) >= Number(req.query.avg_vote)
+    );
+  }
+
+  res.json(response);
+}
+
+app.get("/movie", handleGetMovie);
 
 const PORT = 8000;
 
