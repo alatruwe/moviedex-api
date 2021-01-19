05f1e4615c269789b892b9868558aec6bfa9f66f
adelinel <adeline.latruwe@gmail.com>
2021-01-18 22:15:36 -0800

    added Auth header and API token

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
@@ -9,6 +10,20 @@ const app = express();
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+
+  console.log("validate bearer token middleware");
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+
+  // move to the next middleware
+  next();
+});
 
 function handleGetMovie(req, res) {
   let response = MOVIEDEX;
